e32e8d88fb409482de842b191906a1caa1adccb3
neTpyceB <pvman@inbox.ru>
2017-02-11 23:42:52 +0200

    PHPUnit added

---
+++ src/phpunit_bootstrap.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Updated by devp.eu at 2017.2.11
+ */
+
+define('INC', true);
+define('MODE', 'site');
+
+$_SERVER['HTTP_HOST'] = 'localhost';
+
+require_once __DIR__ . '/../../../../boot.php';
